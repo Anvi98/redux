c4b9fb5316fb8330ddd6b402b7df5ae633ec60af
Anvi Alex <epononanvialex@gmail.com>
2023-01-27 17:30:28 +0200

    add code in App js to count with redux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+// import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { decrement, increment } from './redux/counter';
 
 const App = () => {
+
+  const {count} = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
+      <h2>The Count is</h2>
+      <p>{count}</p>
+      <button onClick={()=> dispatch(increment())}>Increment</button>
+      <button onClick={()=> dispatch(decrement())}>Decrement</button>
     </div>
   );
 }
